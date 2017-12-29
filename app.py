c1390e77c7305982f18dcdb6abbc5eb710910aef
yan1435 <yan1435@gmail.com>
2017-12-29 09:41:28 +0800

    update

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, request, render_template
 
+from config.base import Configuration
+
 app = Flask(__name__)
 
+app.config.from_object(Configuration)
 
-@app.route("/")
+
+@app.route("/", methods=['GET', 'POST'])
 def main():
+    if request.method == 'POST':
+        textFilter = request.form['InputText']  # mengambil data dari form
+        print(textFilter)
+    else:
+        print('tidak di baca')
     return render_template("index.html")
 
 
